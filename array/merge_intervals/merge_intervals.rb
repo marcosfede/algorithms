69c0f991f7070a842770c7a259c16a12c069f84e
Federico Marcos <marcosfede@gmail.com>
2017-05-01 23:38:27 -0300

    cleaned

--- array/merge_intervals/merge_intervals.rb
+++ array/merge_intervals/merge_intervals.rb
@@ -1,10 +1,9 @@
 def merge_intervals(arr)
-
+    arr.sort
 end
 
 
 given = [[1, 3], [2, 6], [8, 10], [15, 18]]
-given_intervals = [Interval(*i) for i in given]
 expected = [[1, 6], [8, 10], [15, 18]]
 p("input: #{given}")
 p("result: #{merge_intervals(given)}")
