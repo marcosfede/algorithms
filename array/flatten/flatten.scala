be44422455038444c45386a54de7df45167321f6
geraq <geraq1988@gmail.com>
2017-05-23 14:46:58 -0300

    flatten in scala with pattern matching

--- array/flatten/flatten.scala
+++ array/flatten/flatten.scala
@@ -1,11 +1,25 @@
-// input = List(2, 1, List(3, List(4, 5), 6), 7, List(8))
+// var input = List(2, 1, List(3, List(4, 5), 6), 7, List(8))
 
+// with guard expressions
 def flatten(input:List[Any]):List[Int] = {
     var result = List[Int]()
     for (x <- input){
         x match {
             case _ if x.isInstanceOf[Int] => result = result :+ x.asInstanceOf[Int]
             case _ if x.isInstanceOf[List[Any]] => result = result ++ flatten(x.asInstanceOf[List[Any]])
+            case _ => throw new IllegalArgumentException("input can only contain ints or lists of ints, with any level of nesting.")
+        }
+    }
+    return result
+}
+
+// with pattern matching
+def flatten2(input:List[Any]):List[Int] = {
+    var result = List[Int]()
+    for (x <- input){
+        x match {
+            case x:Int => result = result :+ x
+            case x:List[Any] => result = result ++ flatten(x)
             case _ => throw new IllegalArgumentException("input can only contain ints or lists of ints, with any level of nesting.")
         }
     }
