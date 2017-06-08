a5f17f06ecb04e371c23c0597f84310e3e685351
geraq <geraq1988@gmail.com>
2017-06-07 22:53:44 -0300

    backtrack/anagram in Haskell.

---
+++ backtrack/anagram/anagram.hs
@@ -0,0 +1,5 @@
+permutations [] = [[]]
+permutations t@(x:xs) = let ixs = zip [1..] t
+                            partialLists = map (\(i,v) -> (v, map snd $ filter ((/= i) . fst) ixs)) ixs
+                        in concatMap (\ (v, ys) -> map (v:) (permutations ys)) partialLists
+
