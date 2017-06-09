dd1b3bb0de1616b9953b3d11dbd64382442a6682
Federico Marcos <marcosfede@gmail.com>
2017-06-09 19:41:27 -0300

    comments

--- backtrack/anagram/anagram.py
+++ backtrack/anagram/anagram.py
@@ -1,8 +1,9 @@
+# lol, probably the most pythonic way
 def perm(word):
     from itertools import permutations
     return permutations(word)
 
 
+# my lazy implementation
 def perm(word):
     yield from (l + x for i, l in enumerate(word) for x in perm(word[:i] + word[i + 1:])) if len(word) > 1 else word
-
