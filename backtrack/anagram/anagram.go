0a278b7d335577405661f1dac8a028bfbfde40a3
Federico Marcos <federico.marcos@celerative.com>
2017-06-11 18:05:35 -0300

    anagram 9 letters

--- backtrack/anagram/anagram.go
+++ backtrack/anagram/anagram.go
@@ -5,14 +5,11 @@ import (
 	"sync"
 )
 
-func perm(word string) []string {
-	if len(word) <= 1 {
-		return []string{word}
-	}
+func multiperm(word string) []string {
 	var list []string
 	var wg sync.WaitGroup
+	wg.Add(len(word))
 	for i, l := range word {
-		wg.Add(1)
 		go func(i int, l rune) {
 			defer wg.Done()
 			subperm := perm(word[:i] + word[i+1:])
@@ -24,8 +21,20 @@ func perm(word string) []string {
 	wg.Wait()
 	return list
 }
-
+func perm(word string) []string {
+	if len(word) <= 1 {
+		return []string{word}
+	}
+	var list []string
+	for i, l := range word {
+		subperm := perm(word[:i] + word[i+1:])
+		for _, c := range subperm {
+			list = append(list, string(l)+c)
+		}
+	}
+	return list
+}
 func main() {
 	word := "abcdefghi"
-	fmt.Println(perm(word))
+	fmt.Println(multiperm(word))
 }
--- backtrack/anagram/anagram.hs
+++ backtrack/anagram/anagram.hs
@@ -3,3 +3,4 @@ permutations t@(x:xs) = let ixs = zip [1..] t
                             partialLists = map (\(i,v) -> (v, map snd $ filter ((/= i) . fst) ixs)) ixs
                         in concatMap (\ (v, ys) -> map (v:) (permutations ys)) partialLists
 
+main = print $ permutations [1..9]
