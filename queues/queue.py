2cba13cba034eef85f36f7a2b538cdba9ee7555a
Federico Marcos <marcosfede@gmail.com>
2018-03-26 17:39:45 -0300

    matrix - unionfind

--- queues/queue.py
+++ queues/queue.py
@@ -20,35 +20,32 @@ class AbstractQueue:
     def __len__(self):
         return self.top
 
-    def __str__(self):
-        result = '------\n'
-        for element in self:
-            result += str(element) + '\n'
-        return result[:-1] + '\n------'
 
-class ArrayQueue(AbstractStack):
+class ArrayQueue(AbstractQueue):
     def __init__(self, size=10):
         """
         Initialize python List with size of 10 or user given input.
         Python List type is a dynamic array, so we have to restrict its
         dynamic nature to make it work like a static array.
         """
-        AbstractStack.__init__(self)
+        AbstractQueue.__init__(self)
         self.array = [None] * size
         self.front = 0
         self.rear = 0
 
     def enqueue(self, value):
-        if self.top == len(self.array):
+        if self.rear == len(self.array):
             self.expand()
-        self.array[self.top] = value
+        self.array[self.rear] = value
+        self.rear += 1
         self.top += 1
 
     def dequeue(self):
         if self.isEmpty():
-            raise IndexError("stack is empty")
-        value = self.array[self.top - 1]
-        self.array[self.top - 1] = None
+            raise IndexError("Queue is empty")
+        value = self.array[self.front]
+        self.array[self.front] = None
+        self.front -= 1
         self.top -= 1
         return value
 
@@ -57,25 +54,27 @@ class ArrayQueue(AbstractStack):
          expands size of the array.
          Time Complexity: O(n)
         """
-        new_array = [None] * len(self.array) * 2 # double the size of the array
+        new_array = [None] * len(self.array) * 2  # double the size of the array
         for i, element in enumerate(self.array):
             new_array[i] = element
         self.array = new_array
 
     def __iter__(self):
-        probe = self.top - 1
+        probe = self.rear
         while True:
             if probe < 0:
                 raise StopIteration
             yield self.array[probe]
             probe -= 1
 
+
 class QueueNode(object):
     def __init__(self, value):
         self.value = value
         self.next = None
 
-class LinkedListQueue(AbstractStack):
+
+class LinkedListQueue(AbstractQueue):
     def __init__(self):
         AbstractQueue.__init__(self)
         self.front = None
@@ -83,7 +82,7 @@ class LinkedListQueue(AbstractStack):
 
     def enqueue(self, value):
         node = QueueNode(value)
-        if not front:
+        if not self.front:
             self.front = node
             self.rear = node
         else:
@@ -96,19 +95,21 @@ class LinkedListQueue(AbstractStack):
             raise IndexError("Queue is empty")
         value = self.front.value
         if self.front is self.rear:
+            self.front = None
             self.rear = None
-        self.front = self.front.next
+        else:
+            self.front = self.front.next
         self.top -= 1
         return value
 
     def __iter__(self):
-        probe = self.head
+        probe = self.rear
         while True:
             if probe is None:
                 raise StopIteration
             yield probe.value
             probe = probe.next
 
-class HeapPriorityQueue(AbstractStack):
-    def __init__(self):
-        pass
+
+class HeapPriorityQueue(AbstractQueue):
+    pass
