7c87d2246d8161b66fa28aaa4967c44ab0b8bc73
Lambdah <eijialford@gmail.com>
2018-04-03 23:57:30 -0700

    Added explanation and example to insertion sort

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -1,7 +1,52 @@
+"""
+Insertion Sort
+Given a list, create two sub lists: sorted list and unsorted list.
+Take an element from the unsorted list adjacent to sorted list and slide it into
+place in the sorted list. Increment the size of the sorted list and decrement
+size of the unsorted list. Continue until sorted list is the size of the list.
+
+https://en.wikipedia.org/wiki/Insertion_sort
+Complexity: O(n^2)
+eg
+<- Position of iterator i
+[36, 14, 20, 19] Initial list
+
+[36<-, 14, 20, 19]
+Set pos := 0
+pos == 0
+    break while loop
+
+[36, 14<-, 20, 19]
+Set pos := 1
+[14, 36<-, 20, 19] 14 < 36, slide 14 down sorted list
+Set pos := 0
+pos == 0
+    break while loop
+
+[14, 36, 20<-, 19]
+Set pos := 2
+[14, 20, 36<-, 19] 20 < 36, slide 20 down sorted list
+Set pos := 1
+[14, 20, 36<-, 19]
+20 > 14
+    break while loop
+
+[14, 20, 36, 19<-]
+Set pos:= 3
+[14, 20, 19, 36<-] 19 < 36, slide 19 down sorted list
+Set pos := 2
+[14, 19, 20, 36<-] 19 < 20, slide 19 down sort list
+Set pos := 1
+[14, 19, 20, 36<-]
+19 > 14
+    break while loop
+i == length of the list
+    break for loop
+
+[14, 19, 20, 36] Sorted list
+"""
+
 def insertion_sort(arr):
-    """ Insertion Sort
-        Complexity: O(n^2)
-    """
     for i, cursor in enumerate(arr):
         pos = i
         while pos > 0 and arr[pos - 1] > cursor:
