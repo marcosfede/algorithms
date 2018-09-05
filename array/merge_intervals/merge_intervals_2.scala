1ff33b3f347bac0d48442e68fc82837820b698c4
Ivan Grcevic <igrcevic@despegar.com>
2018-09-05 15:48:03 -0300

    fixed typo in "ms"

--- array/merge_intervals/merge_intervals_2.scala
+++ array/merge_intervals/merge_intervals_2.scala
@@ -4,7 +4,7 @@ object MergeIntervals extends App {
     val t0 = System.currentTimeMillis()
     val result = block // call-by-name
     val t1 = System.currentTimeMillis()
-    println("Elapsed time: " + (t1 - t0) + "ns")
+    println("Elapsed time: " + (t1 - t0) + "ms")
     result
   }
 
