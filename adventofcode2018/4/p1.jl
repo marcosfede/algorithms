e575be21b4839a5fc5b5ff09b9706882d05597d9
Federico Marcos <federico.marcos@celerative.com>
2018-12-05 01:19:53 -0300

    wip d4

--- adventofcode2018/4/p1.jl
+++ adventofcode2018/4/p1.jl
@@ -6,15 +6,40 @@ parseInt = x -> parse(Int, x)
 function parse_line(line::String)
   regex = r"\[(?<year>\d+)-(?<month>\d+)-(?<day>\d+) (?<hour>\d+):(?<minute>\d+)\] (?<wakes>wakes up)*(?<sleep>falls asleep)*(?<shift>Guard #(?<gid>\d+) begins shift)*"
   m = match(regex, line)
-  date = DateTime(m[:year], m[:month], m[:day], m[:hour], m[:minute])
+
+  date = (m[:year], m[:month], m[:day], m[:hour], m[:minute]) .|> parseInt |> x -> DateTime(x...)
   return (date, m[:wakes], m[:sleep], m[:gid])
 end
 
 function solve(lines::Vector{String})
-
+  events = []
+  by_id = Map()
+  # parse input
   for line in lines
-    
+    (date, wakes, sleep, gid) = parse_line(line)
+    push!(events, wakes, sleep, gid)
   end
+  # sort dates
+  sort!(events, by=x->x[1])
+
+  # group by id
+  for event in events:
+    (date, wakes, sleep, gid) = event
+    if gid != nothing
+      current_id = gid
+    end
+    push!(by_id[current_id], event)
+  end
+
+  # parse into bitarray
+
+  # sum over bitarrays
+
+  # find max sum ID
+
+  # find num intersections between bitarrays
+
+  # return ID * numintersections
 end
 
 println(solve(lines))
