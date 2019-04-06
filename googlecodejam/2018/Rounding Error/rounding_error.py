b1322f0b3840adc5a63a0fc0a95206b2ca7cd88a
Federico Marcos <federico.marcos@celerative.com>
2019-04-06 19:52:12 -0300

    gcj 2018 problems

--- googlecodejam/2018/Rounding Error/rounding_error.py
+++ googlecodejam/2018/Rounding Error/rounding_error.py
@@ -1,7 +1,7 @@
-# cuanto vale cada voto en su parte decimal, *100 y sacas parte entera
-#
-
 import math
+
+# solution does not pass the last test case...
+
 
 def solve(npersons, nlanguages, langvalues_original):
     votevalue = 100/npersons
@@ -10,18 +10,22 @@ def solve(npersons, nlanguages, langvalues_original):
         return 100
     accum = 0
 
-    langvalues = [x*votevalue for x in (langvalues_original + [0]*(npersons - sum(langvalues_original)))]
+    langvalues = [
+        x*votevalue for x in (langvalues_original + [0]*(npersons - sum(langvalues_original)))]
 
     missing_people = []
 
-    langvalues_decimals = list(map(lambda lang: lang - math.floor(lang), langvalues))
+    langvalues_decimals = list(
+        map(lambda lang: lang - math.floor(lang), langvalues))
     for idx, lang in enumerate(langvalues_decimals):
         if lang < 0.5:
             accum += math.floor(langvalues[idx])
-            missing_people.append((math.ceil((0.5-lang)/decimal_votevalue), lang))
+            missing_people.append(
+                (math.ceil((0.5-lang)/decimal_votevalue), lang))
         else:
             accum += math.ceil(langvalues[idx])
-            missing_people.append((math.ceil((1.5-lang)/decimal_votevalue), lang))
+            missing_people.append(
+                (math.ceil((1.5-lang)/decimal_votevalue), lang))
 
     # missing_people += [0.5/decimal_votevalue]*(npersons - sum(langvalues))
 
@@ -32,14 +36,17 @@ def solve(npersons, nlanguages, langvalues_original):
         if remaining >= missing_people_ordered[0][0]:
             remaining -= missing_people_ordered[0][0]
             accum += 1
-            nueva_suma = missing_people_ordered[1][0] + missing_people_ordered[0][0]*decimal_votevalue
+            nueva_suma = missing_people_ordered[1][0] + \
+                missing_people_ordered[0][0]*decimal_votevalue
             nueva_suma = nueva_suma - math.floor(nueva_suma)
             accum += math.floor(missing_people_ordered[0][0]*votevalue)
             if nueva_suma < 0.5:
-                nuevaTupla = (math.ceil((0.5-nueva_suma)/decimal_votevalue), nueva_suma)
+                nuevaTupla = (math.ceil((0.5-nueva_suma) /
+                                        decimal_votevalue), nueva_suma)
 
             else:
-                nuevaTupla = (math.ceil((1.5-lang)/decimal_votevalue), nueva_suma)
+                nuevaTupla = (
+                    math.ceil((1.5-lang)/decimal_votevalue), nueva_suma)
             missing_people_ordered.append(nuevaTupla)
             missing_people_ordered.pop(0)
 
@@ -49,10 +56,6 @@ def solve(npersons, nlanguages, langvalues_original):
             break
 
     return accum
-
-
-
-
 
 
 def read_input():
