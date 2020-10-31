7a07a5b8ec65c7d3b6f6498abf5fdcf5a26367e2
Federico Marcos <marcosfede@gmail.com>
2020-10-31 12:59:54 -0300

    all factors (#60)

---
+++ dfs/all_factors/all_factors.md
@@ -0,0 +1,24 @@
+Write a function that takes an integer n and return all possible combinations
+of its factors (other than 1 and itself).
+
+Numbers can be regarded as product of its factors. For example,
+8 = 2 x 2 x 2;
+  = 2 x 4.
+
+Examples:
+input: 1
+output:
+[]
+
+
+input: 37
+output:
+[]
+
+input: 32
+output:
+[
+  [2, 16],
+  [2, 2, 8],
+  [2, 2, 2, 4],
+  [2, 2, 2, 2, 2],
--- dfs/all_factors/all_factors.py
+++ dfs/all_factors/all_factors.py
@@ -1,34 +1,3 @@
-"""
-Numbers can be regarded as product of its factors. For example,
-8 = 2 x 2 x 2;
-  = 2 x 4.
-
-
-Write a function that takes an integer n and return all possible combinations
-of its factors.Numbers can be regarded as product of its factors. For example,
-8 = 2 x 2 x 2;
-  = 2 x 4.
-
-Examples:
-input: 1
-output:
-[]
-
-
-input: 37
-output:
-[]
-
-input: 32
-output:
-[
-  [2, 16],
-  [2, 2, 8],
-  [2, 2, 2, 4],
-  [2, 2, 2, 2, 2],
-"""
-
-
 def get_factors(n):
     def factor(n, i, combi, res):
         while i * i <= n:
